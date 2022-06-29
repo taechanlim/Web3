2de5d1f0eee5a6ca513a95ab8bc222e2bd538cf6
taechanlim <tccap123@naver.com>
2022-06-29 12:47:02 +0900

    수업

--- front/src/App.js
+++ front/src/App.js
@@ -5,6 +5,14 @@ function App() {
   const [account, web3] = useWeb3();
   const [isLogin, setIsLogin] = useState(false);
   const [balance, setBalance] = useState(0);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    await web3.eth.sendTransaction({
+      from: account,
+      to: e.target.received.value,
+      value: web3.utils.toWei(e.target.amount.value, "ether"),
+    });
+  };
 
   //useWeb3에서 계정 정보 값을 가져와진다면 setIsLogin값을 true로 바꾼다
   useEffect(() => {
@@ -24,6 +32,13 @@ function App() {
         <h2>{account}님 환영합니다.</h2>
         <div>Balance : {balance} ETH</div>
       </div>
+
+      <form onSubmit={handleSubmit}>
+        <input type="text" id="received" placeholder="받을계정"></input>
+        <input type="number" id="amount" placeholder="보낼 금액"></input>
+        ETH
+        <input type="submit" value="전송"></input>
+      </form>
     </div>
   );
 }
